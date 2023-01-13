2f0d9d15e0d7327ca591b0f80e7c13efe86a7e6f
pomdtr <achille.lacoin@gmail.com>
2023-01-13 22:57:29 +0100

    remove macos specific config

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -6,10 +6,10 @@
 fn main() {
     fix_path_env::fix().expect("failed to fix PATH environment variable");
     tauri::Builder::default()
-        .setup(|app| {
-            app.set_activation_policy(tauri::ActivationPolicy::Accessory); // Disable the icon in the dock
-            Ok(())
-        })
+        // .setup(|app| {
+        //     app.set_activation_policy(tauri::ActivationPolicy::Accessory); // Disable the icon in the dock
+        //     Ok(())
+        // })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
